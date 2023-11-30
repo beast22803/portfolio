a6015f5e25daca47209d65a29c4bc8b8d28faaa1
Varshit Madisetti <varshit.madi@gmail.com>
2023-11-30 19:46:04 +0530

    Random Commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,10 +6,10 @@ function Home() {
         <>
             <div className="HomeDiv d-flex justify-content-center align-items-center">
                 <div className="backImg"></div>
-                <div className="childDiv text-center">
+                <div className="childDiv text-center mb-5">
                     <div><h1 className="nameHead">VARSHIT MADISETTI</h1></div>
                     <div>
-                        <h3 className="roleHead mt-4">SOFTWARE ENGINEER, FULL STACK & APP DEVELOPER.</h3>
+                        <h3 className="roleHead mt-2">SOFTWARE ENGINEER, FULL STACK & APP DEVELOPER.</h3>
                     </div>
                 </div>
             </div>
--- src/Components/Navbaar/Navbared.jsx
+++ src/Components/Navbaar/Navbared.jsx
@@ -10,24 +10,23 @@ function Navbared(props) {
     return (
         <>
             <Navbar fixed="top" expand="lg">
-                <Container className="navv justify-content-between align-items-center m-0">
-                    {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-                    <Navbar.Brand href="" className="navBrand m-0 ms-3">
+                <Container className={`navv justify-content-between align-items-center m-0`}>
+                    <Navbar.Brand href="" className={`navBrand m-0 ms-3 ${width < 900 && "mt-3"} `}>
                         <Typewriter words={['<VARSHIT MADISETTI/>']} />
                     </Navbar.Brand>
-                    {/* <div>
+                    <div>
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="ms-3">
-                                <Nav.Link className={`navLink mx-2 ${width < 900 ? `my-0` : `my-4`}`} href="about">// about</Nav.Link>
-                                <Nav.Link className={`navLink mx-2 ${width < 900 ? `my-0` : `my-4`}`} href="expertise">// expertise</Nav.Link>
-                                <Nav.Link className={`navLink mx-2 ${width < 900 ? `my-0` : `my-4`}`} href="projects">// projects</Nav.Link>
-                                <Nav.Link className={`navLink mx-2 ${width < 900 ? `my-0` : `my-4`}`} href="experience">// experience</Nav.Link>
-                                <Nav.Link className={`navLink mx-2 ${width < 900 ? `my-0` : `my-4`}`} href="contact">// contact</Nav.Link>
+                                <Nav.Link className={`navLink mx-2 my-4`} href="about">// about</Nav.Link>
+                                <Nav.Link className={`navLink mx-2 my-4`} href="expertise">// expertise</Nav.Link>
+                                <Nav.Link className={`navLink mx-2 my-4`} href="projects">// projects</Nav.Link>
+                                <Nav.Link className={`navLink mx-2 my-4`} href="experience">// experience</Nav.Link>
+                                <Nav.Link className={`navLink mx-2 my-4`} href="contact">// contact</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                     </div>
                     <div></div>
-                    <div></div> */}
+                    <div></div>
                 </Container>
             </Navbar >
         </>
